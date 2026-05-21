bdbeef1fb62be3dcd55334ead50b520878cc6624
Rotfuchs-von-Vulpes <derfuchs.pa@gmail.com>
2026-05-20 23:45:17 -0400

    clean

--- app/textureDraw/tools/pencil/pencil.go
+++ app/textureDraw/tools/pencil/pencil.go
@@ -5,35 +5,35 @@ type Pencil struct{}
 var painting bool
 var painted [][2]int32
 
-func (s Pencil) SendTexture(colors [][][4]float32, width, height uint32) {
+func (_ Pencil) SendTexture(colors [][][4]float32, width, height uint32) {
 
 }
 
-func (s Pencil) ButtonPress(pos [2]int32) {
+func (_ Pencil) ButtonPress(pos [2]int32) {
 	painting = true
 }
 
-func (s Pencil) ButtonRelease(pos [2]int32) {
+func (_ Pencil) ButtonRelease(pos [2]int32) {
 	painting = false
 }
 
-func (s Pencil) Move(pos1, pos2 [2]int32) {
+func (_ Pencil) Move(pos1, pos2 [2]int32) {
 	if painting {
 		painted = append(painted, pos2)
 	}
 }
 
-func (s Pencil) Visualize() [][2]int32 {
+func (_ Pencil) Visualize() [][2]int32 {
 	return painted
 }
 
-func (s Pencil) Change() (toChange [][2]int32) {
+func (_ Pencil) Change() (toChange [][2]int32) {
 	toChange = painted
 	painted = make([][2]int32, 0)
 	return
 }
 
-func (s Pencil) Reset() {
+func (_ Pencil) Reset() {
 	painting = false
 	painted = make([][2]int32, 0)
 }
